fa85f6126e5dd49de37194637ca1b044270a4de5
Vivian-kawei <505495084@qq.com>
2017-06-13 21:43:56 +0800

    6.13

---
+++ src/store/modules/affirm.js
@@ -0,0 +1,26 @@
+export default {
+  state: {
+    memberList: [{
+      name: '陈记三及第（科技园店）',
+      status: 0 // 0未报名,1已报名,2不参加
+    }, {
+      name: '贡茶',
+      status: 1
+    }, {
+      name: '一点点',
+      status: 1
+    }, {
+      name: '大家乐',
+      status: 2
+    }, {
+      name: 'KFC',
+      status: 1
+    }, {
+      name: '麦当劳',
+      status: 2
+    }, {
+      name: '海记',
+      status: 0
+    }]
+  }
+};
